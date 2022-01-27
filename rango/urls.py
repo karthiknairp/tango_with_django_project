0018c50fe660191b9dd757bb4bc9af47b2642404
Karthik Nair <2704237n@student.gla.ac.uk>
2022-01-27 21:41:22 +0000

    Upto Chapter 4

--- rango/urls.py
+++ rango/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.urls import path
 from rango import views
 
+app_name = 'rango'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about')
 ]
--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
-# Create your views here.
 
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html',)
